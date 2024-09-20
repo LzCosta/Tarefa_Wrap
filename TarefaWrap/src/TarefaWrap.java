65f07d78b4dd7695805e174266b60244095f4df2
LzCosta <116127270+LzCosta@users.noreply.github.com>
2024-09-20 14:48:52 -0300

    Add files via upload

---
+++ TarefaWrap/src/TarefaWrap.java
@@ -0,0 +1,15 @@
+public class TarefaWrap {
+
+    public static void main(String[] args) {
+
+        int exemplo = 5;
+
+        System.out.println("Valor primitivo: "+exemplo);
+
+        Integer exemplo1 = exemplo;
+
+        System.out.println("Classe Wrapper: "+exemplo1);
+
+
+    }
+}
